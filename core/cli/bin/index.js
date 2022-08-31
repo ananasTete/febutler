fd47f54774e5e72507c6b07482e9760a1acd5e4b
李子航 <2045268468@qq.com>
2022-08-31 16:57:04 +0800

    使用 import-local 库判断是否启用本地的脚手架

--- core/cli/bin/index.js
+++ core/cli/bin/index.js
@@ -1,1 +1,9 @@
 #!/usr/bin/env node
+
+const importLocal = require('import-local');
+
+if (importLocal(__filename)) {
+  require('npmlog').info('cli', '正在使用 febutler 的本地版本！')
+} else {
+  require('../lib/cli')(process.argv.slice(2))
+}
